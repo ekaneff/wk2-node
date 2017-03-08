ef017c29942c58c105e2607bbefdc422809e58b2
Emily Kaneff <emilykaneff@Emilys-MacBook-Pro.local>
2017-03-07 23:52:36 -0500

    most of setup finished, working on node script

---
+++ node.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd ~
+curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install nodejs
+sudo apt-get install build-essential -y
+sudo npm install -g pm2
